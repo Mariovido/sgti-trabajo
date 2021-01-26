0f98287eba14a9e6997f81f37e91b792c32d0ee6
Mario <mavido3@alumno.upv.es>
2021-01-26 21:27:49 +0100

    Arreglos en los servlets y corrección de errores

--- src/java/login.java
+++ src/java/login.java
@@ -1,15 +1,12 @@
 import java.io.*;
 import java.sql.*;
-import javax.http.*;
-import javax.http.servlet.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-
-
-public class login extends HttpServlet{
-    public void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException, ServletException{
+public class Login extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         RequestDispatcher rd = req.getRequestDispatcher("web/login.html");
         rd.forward(req, res);
-
 
         Connection con;
         Statement st;
@@ -17,31 +14,36 @@ public class login extends HttpServlet{
         String SQL, user, pass;
         PrintWriter out;
 
-        user=req.getParameter("user");
-        pass=req.getParameter("pass");
-        try{class.forName("com.mysql.jdbc.Driver");
-    } catch(Exception e){
-        out.println("<div> No se pudo cargar el puente jdbc </div>");
-    }
-
-    try{ 
-        con = DriverManager.getConnection("jdbc:mysql://");
-        st= con.createStatement();
-        SQL="SELECT * FROM usuarios WHERE usuarios.Nombre='"+user+"' AND usuarios.constrasena='"+pass+"'";
-        rs=st.executeQuery(SQL);
-
-        if(!rs.next()){
-            // si no hay coincidencia se vuelve a inicio
-            response.sendRedirect("http://localhost:8080/sgti-trabajo/inicio");
-
-        }else{
-            // si hay un usuario se crea una variable de sessión
-            HttpSession misesion = req.getSession(true);
-            // y se redirige a la pantalla de registro de partidas iniciadas
-            response.sendRedirect("http://localhost:8080/sgti-trabajo/")
-            
+        user = req.getParameter("USER");
+        pass = req.getParameter("PASS");
+        
+        out = res.getWriter();
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch(Exception e) {
+            out.println("<div> No se pudo cargar el puente jdbc </div>");
         }
-    }catch(Exception e){
-        out.println("<div> Error " +e+ "</div>");
+
+        try { 
+            con = DriverManager.getConnection("jdbc:mysql://");
+            st= con.createStatement();
+            SQL="SELECT * FROM Usuarios WHERE Nick='" + user + "' AND Contraseña='" + pass + "'";
+            rs=st.executeQuery(SQL);
+
+            if(!rs.next()) {
+                // si no hay coincidencia se vuelve a inicio
+                res.sendRedirect("http://localhost:8080/sgti-trabajo/inicio");
+
+            } else {
+                // si hay un usuario se crea una variable de sessión
+                HttpSession misesion = req.getSession(true);
+                // y se redirige a la pantalla de registro de partidas iniciadas
+                res.sendRedirect("http://localhost:8080/sgti-trabajo/principal");
+
+            }
+        }catch(Exception e){
+            out.println("<div> Error " + e + "</div>");
+        }
     }
 }
