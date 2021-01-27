62802fb31dcc72526720d8455c81dc13e8e4410b
Mario <mavido3@alumno.upv.es>
2021-01-27 10:20:57 +0100

    Nuevos servlets

---
+++ src/java/Cuenta.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Cuenta extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        PrintWriter out = res.getWriter();
+        try {
+            HttpSession sesion = req.getSession(false);
+
+            if(sesion!=null) {
+                RequestDispatcher rd = req.getRequestDispatcher("web/cuenta.html");
+                rd.forward(req, res);
+            } else {
+                res.sendRedirect("http://localhost:8080/sgti-trabajo/inicio");
+            }
+        } catch(Exception e){
+            out.println("<div> Error " + e + "</div>");
+        }
+    }
+}
