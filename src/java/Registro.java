47ffe7d2dc0d7a517613488048ea512d833c03f3
Mario <mavido3@alumno.upv.es>
2021-02-04 11:02:22 +0100

    Registro.java

--- src/java/Registro.java
+++ src/java/Registro.java
@@ -4,10 +4,11 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class Registro extends HttpServlet {
-    public void doPost(HttpServletRequest req, HttpServletResponse res) /*throws IOException, ServletException*/ {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) {
         Connection con;
-        Statement st,st2;
-        ResultSet rs2;
+        Statement st;
+        ResultSet rs;
+        PreparedStatement ps;
         String SQL,SQL2, nick, correo, pass, metodo, nombre;
         byte conectado;
         PrintWriter out;
@@ -26,53 +27,39 @@ public class Registro extends HttpServlet {
         try{
             out = res.getWriter();
        
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch(Exception e) {
-            out.println("<div> No se pudo cargar el puente jdbc </div>");
-        }
-
-               //try { 
-            
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuatroenraya?serverTimezone=UTC","root","1234");
-            if (con==null){
-                out.println("<div>no hay conexion</div>");
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch(Exception e) {
+                out.println("<div> No se pudo cargar el puente jdbc </div>");
             }
             
-            //if(con!=null){
-               // out.println("<div> hay conexion con bbdd </div>");
-            //}
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuatroenraya?serverTimezone=UTC","root","1234");
+
             con.setAutoCommit(false);
-            st = con.createStatement();
-            SQL="INSERT INTO Usuarios (Nick, Correo, Contraseña, Metodo, Nombre, Conectado) VALUES ('" + nick + "', '" +
+            SQL="INSERT INTO Usuarios (Nick, Correo, Contraseï¿½a, Metodo, Nombre, Conectado) VALUES ('" + nick + "', '" +
             correo + "', '" + pass + "', '" + metodo + "', '" + nombre + "', " + conectado + ")";
-            
-                //aqui se inserta el nuevo usuario que se ha registrado
-                result =st.executeUpdate(SQL); // si result es 1 significa que se ha hecho bien el insert
-                con.commit();
-                con.setAutoCommit(true);
+            ps = con.prepareStatement(SQL);
+            result = ps.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
           
-                // si hay un usuario se crea una variable de sessión
-                HttpSession misesion = req.getSession(true);
+            // si hay un usuario se crea una variable de session
+            HttpSession misesion = req.getSession(true);
                 
-                // creamos la SQL para recoger el IdUsuario
-                st2 = con.createStatement();
-                SQL2 = "SELECT * FROM Usuarios WHERE Usuarios.Nick='"+nick+"'";
-                rs2 = st2.executeQuery(SQL2);
-               //rs2.next();
-               if(rs2==null){
-                   out.println("<div> el objeto rs2 es nulo </div>");
-                }
-               while(!rs2.next()){
-                   misesion.setAttribute("IdUsuario", rs2.getString(1));
-               }
-                rs2.close();
-                // y se redirige a la pantalla de registro de partidas iniciadas
-                st.close();
-                st2.close();
-                con.close();
-              out.close();
-            res.sendRedirect("http://localhost:8080/sgti-trabajo/principal");
+            // creamos la SQL para recoger el IdUsuario
+            st = con.createStatement();
+            SQL2 = "SELECT * FROM Usuarios WHERE Usuarios.Nick='"+nick+"'";
+            rs = st.executeQuery(SQL2);
+
+            while(!rs.next()){
+                misesion.setAttribute("IdUsuario", rs.getString(1));
+            }
+            ps.close();
+            rs.close();
+            st.close();
+            con.close();
+            out.close();
+            res.sendRedirect("/sgti-trabajo/principal");
                 
         } catch(Exception e){
             System.out.println( "<div> Hay un error en el codigo </div>"+ e );
