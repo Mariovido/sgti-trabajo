fd756325626c32bfc76b92f79c5c2cfe7ddddf3c
Mario <mavido3@alumno.upv.es>
2021-02-04 12:12:12 +0100

    Registro.java con formulario GET.3

--- src/java/Registro.java
+++ src/java/Registro.java
@@ -6,10 +6,8 @@ import javax.servlet.http.*;
 public class Registro extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse res) {
         Connection con;
-        Statement st;
-        ResultSet rs;
         PreparedStatement ps;
-        String SQL,SQL2, nick, correo, pass, metodo, nombre;
+        String SQL, nick, correo, pass, metodo, nombre;
         byte conectado;
         PrintWriter out;
         int result =0;
@@ -36,7 +34,7 @@ public class Registro extends HttpServlet {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuatroenraya?serverTimezone=UTC","root","1234");
 
             con.setAutoCommit(false);
-            SQL="INSERT INTO Usuarios (Nick, Correo, Contrase�a, Metodo, Nombre, Conectado) VALUES ('" + nick + "', '" +
+            SQL="INSERT INTO Usuarios (Nick, Correo, Contraseña, Metodo, Nombre, Conectado) VALUES ('" + nick + "', '" +
             correo + "', '" + pass + "', '" + metodo + "', '" + nombre + "', " + conectado + ")";
             ps = con.prepareStatement(SQL);
             result = ps.executeUpdate();
@@ -45,26 +43,14 @@ public class Registro extends HttpServlet {
           
             // si hay un usuario se crea una variable de session
             HttpSession misesion = req.getSession(true);
-                
-            // creamos la SQL para recoger el IdUsuario
-            st = con.createStatement();
-            SQL2 = "SELECT * FROM Usuarios WHERE Usuarios.Nick='"+nick+"'";
-            rs = st.executeQuery(SQL2);
 
             res.setContentType("text/html");
             out.println("<HTML><BODY>");
-            while(!rs.next()){
-                out.println("<DIV>El registro ha sido un éxito clicke para continuar</DIV>");
-                out.println("<FORM ACTION = '/sgti-trabajo/principal' METHOD = 'GET'>");
-                out.println("<SELECT NAME = 'IdUsuario' VALUE ='" + rs.getInt(1) + "'><BR>");
-                out.println("<INPUT TYPE = 'SUBMIT' VALUE = 'CONTINUAR'>");
-                misesion.setAttribute("IdUsuario", rs.getInt(1));
-            }
-            out.println("<DIV>No se ha podido realizar el registro</DIV>");
+            out.println("<DIV>El registro ha sido un éxito clicke para continuar</DIV>");
+            out.println("<FORM ACTION = '/sgti-trabajo/principal' METHOD = 'GET'>");
+            out.println("<INPUT TYPE = 'SUBMIT' VALUE = 'CONTINUAR'>");
             out.println("</FORM></BODY></HTML>");
             ps.close();
-            rs.close();
-            st.close();
             con.close();
             out.close();
         } catch(Exception e){
