695159807a877c6bfdff456e884bc2f60cb32b69
unknown <maria.msimo@gmail.com>
2021-02-12 18:37:51 +0100

    arreglado cuenta

--- src/java/Cuenta.java
+++ src/java/Cuenta.java
@@ -8,7 +8,8 @@ public class Cuenta extends HttpServlet {
         Connection con;
         Statement st;
         ResultSet rs;
-        String SQL, IdUsuario;
+        String SQL;
+        int IdUsuario;
         PrintWriter out;
 
         out = res.getWriter();
@@ -16,60 +17,66 @@ public class Cuenta extends HttpServlet {
             HttpSession sesion = req.getSession(false);
 
             if(sesion!=null) {
-                IdUsuario = (String)sesion.getAttribute("IdUsuario");
+                //IdUsuario = String.valueOf(sesion.getAttribute("IdUsuario"));
+                IdUsuario= (int) sesion.getAttribute("IdUsuario");
+                
                 Class.forName("com.mysql.jdbc.Driver");
 
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuatroenraya?serverTimezone=UTC","root","1234");
-            if (con==null){
-                out.println("<div>no hay conexion</div>");
-            }
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuatroenraya?serverTimezone=UTC","root","1234");
+                if (con==null){
+                    out.println("<div>no hay conexion</div>");
+                }
                 st = con.createStatement();
-                SQL = "SELECT * FROM Usuarios WHERE Usuarios.IdUsuario = " + IdUsuario;
+                SQL = "SELECT * FROM Usuarios WHERE IdUsuario =" +IdUsuario;
                 rs=st.executeQuery(SQL);
 
-                // Hacemos el HTML
-                out.println("<!DOCTYPE html>");
-                out.println("<html lang='en'>");
-                out.println("<head>");
-                out.println("    <meta charset='UTF-8'>");
-                out.println("    <meta name='viewport' content='width=device-width, initial-scale=1.0'>");
-                out.println("    <title>Cuatro En Raya</title>");
-                out.println("    <link rel='stylesheet' href='resources/styles/main.css'>");
-                out.println("    <link rel='stylesheet' href='resources/styles/registro.css'>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("    <header class='main-header'>");
-                out.println("        <nav class='main-header__nav'>");
-                out.println("            <ul class='main-header__item-list'>");
-                out.println("                <li class='main-header__item'><form method='POST' action='/sgti-trabajo/principal'><button type='submit'>Mis partidas</button></form></li>");
-                out.println("                <li class='main-header__item'><a class='active' href=''>Mi cuenta</a></li>");
-                out.println("            </ul>");
-                out.println("        </nav>");
-                out.println("    </header>");
-                out.println("    <main>");
-                out.println("        <div class='user'>");
-                out.println("            <header class='user__header'>");
-                out.println("                <h1 class='user__title'>Datos de la Cuenta</h1>");
-                out.println("            </header>");
-                out.println("            <div name='formulario-login' class='form'>");
-                out.println("                <div class='form__group'>");
-                out.println("                    <span class='form__input'>Nombre: " + rs.getString(6) + "</span>");
-                out.println("                </div>");
-                out.println("                <div class='form__group'>");
-                out.println("                    <span class='form__input'>Correo: " + rs.getString(3) + "</span>");
-                out.println("                </div>");
-                out.println("                <div class='form__group'>");
-                out.println("                    <span class='form__input'>Id: " + rs.getString(1) + "</span>");
-                out.println("                </div>");
-                out.println("            </div>");
-                out.println("            </div>");
-                out.println("    </main>");
-                out.println("</body>");
-                out.println("</html>");
-                
-                rs.close();
-                st.close();
-                con.close();
+                if(!rs.next()){
+                    res.sendRedirect("http://juegocraya.duckdns.org:8080/sgti-trabajo/inicio");
+                }else{
+                    // Hacemos el HTML
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html lang='en'>");
+                    out.println("<head>");
+                    out.println("    <meta charset='UTF-8'>");
+                    out.println("    <meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+                    out.println("    <title>Cuatro En Raya</title>");
+                    out.println("    <link rel='stylesheet' href='web/resources/styles/main.css'>");
+                    out.println("    <link rel='stylesheet' href='web/resources/styles/registro.css'>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("    <header class='main-header'>");
+                    out.println("        <nav class='main-header__nav'>");
+                    out.println("            <ul class='main-header__item-list'>");
+                    out.println("                <li class='main-header__item'><form method='POST' action='/sgti-trabajo/principal'><button type='submit'>Mis partidas</button></form></li>");
+                    out.println("                <li class='main-header__item'><a class='active' href=''>Mi cuenta</a></li>");
+                    out.println("            </ul>");
+                    out.println("        </nav>");
+                    out.println("    </header>");
+                    out.println("    <main>");
+                    out.println("        <div class='user'>");
+                    out.println("            <header class='user__header'>");
+                    out.println("                <h1 class='user__title'>Datos de la Cuenta</h1>");
+                    out.println("            </header>");
+                    out.println("            <div name='formulario-login' class='form'>");
+                    out.println("                <div class='form__group'>");
+                    out.println("                    <span class='form__input'>Nombre: " + rs.getString(6) + "</span>");
+                    out.println("                </div>");
+                    out.println("                <div class='form__group'>");
+                    out.println("                    <span class='form__input'>Correo: " + rs.getString(3) + "</span>");
+                    out.println("                </div>");
+                    out.println("                <div class='form__group'>");
+                    out.println("                    <span class='form__input'>Id: " + rs.getString(1) + "</span>");
+                    out.println("                </div>");
+                    out.println("            </div>");
+                    out.println("            </div>");
+                    out.println("    </main>");
+                    out.println("</body>");
+                    out.println("</html>");
+
+                    rs.close();
+                    st.close();
+                    con.close();
+                }
             } else {
                 out.close();
                 res.sendRedirect("http://localhost:8080/sgti-trabajo/inicio");
