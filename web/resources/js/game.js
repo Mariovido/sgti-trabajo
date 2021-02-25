1b9aff36468d3969ecdde8f60a5b223b0b601989
Mario <mavido3@alumno.upv.es>
2021-02-25 18:35:35 +0100

    game.js cambio

--- web/resources/js/game.js
+++ web/resources/js/game.js
@@ -4,12 +4,12 @@ function paint(){
     id = paint.caller.arguments[0].target.id;
     NFila = parseInt(id.charAt(0));
     NCol = parseInt(id.charAt(1));
-    col = matrizSt.substring(8*NCol,7+8*NCol);
+    col = matrizSt.substring(7*NCol,7+7*NCol);
     for (var i=6; i>=0; i--){
         if (col.charAt(i) == "0"){
             col = col.replaceAt(i, "1");
             document.getElementById(i.toString()+NCol.toString()).className = "j1";
-            matrizSt=matrizSt.replaceAt(NCol*8+i,"1");
+            matrizSt=matrizSt.replaceAt(NCol*7+i,"1");
             break;
         }
     }
@@ -20,9 +20,9 @@ String.prototype.replaceAt = function(index, replacement) {
 }
 
 function paintInit(matriz){
-    for (var j=0; j<8; j++){
+    for (var j=0; j<7; j++){
         //col = matrizSt.substring(7*j,7+7*j);
-        col = matriz.substring(8*j,7+8*j);
+        col = matriz.substring(7*j,7+7*j);
         for (var i = 0; i<7; i++){
             if(col.charAt(i) == "1"){
                 document.getElementById(i.toString()+j.toString()).className = "j1";
